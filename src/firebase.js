2c92dba9de7b33b9c7c77a1a3d987a664530ab00
kweinerman <kurt.weinerman@gmail.com>
2024-09-06 20:46:49 -0600

    updated config

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getDatabase, ref, set, onValue } from 'firebase/database';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCCLIv3cW6p-0lqyrcqoeOEp48NNOwZUnE",
-  authDomain: "touchdown-scorer.firebaseapp.com",
-  projectId: "touchdown-scorer",
-  storageBucket: "touchdown-scorer.appspot.com",
-  messagingSenderId: "539238756743",
-  appId: "1:539238756743:web:1e93b22019985f4ad86322",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: "G-M6PV575W13"
 };
 
